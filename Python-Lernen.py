cca76f26bb557e37cf4022f3d8a672e44e176d93
Fabian Wiedemann <fabian.wiedemann6@gmail.com>
2025-03-01 15:39:37 +0100

    Funktion für falsche Lösungen ohne Bedingung, dass man nur 5 Versuche hat

--- Python-Lernen.py
+++ Python-Lernen.py
@@ -1,6 +1,6 @@
 #Authors: Moritz Mezler, Fabian Wiedemann
 #Matrikelnummer von Moritz Mezler:
-#Matrikelnummer von Fabian Wiedemann:
+#Matrikelnummer von Fabian Wiedemann: 6415375
 #date: 2025-03-01
 
 'Programm to learn basic skills in Python'
@@ -9,26 +9,27 @@
 ### Values ###
 
 ### Functions ###
-def check_answer (response):
-    user_response = input("Deine Lösung: ")
-    response_tries = 5
-    while user_response != response:
-        if response_tries > 0:
-            response_tries -= 1 
-            print("Falsche Antwort!")
-            print(f"du hast noch {response_tries} Versuche!")
+def check_answer (response):                                    #überprüft, ob die Eingabe der jeweiligen Aufgabe richtig ist.
+    user_response = input("Deine Lösung: ")                     #Wenn Eingabe nicht richtig ist, wird die Anzahl der Versuche angezeigt
+    response_tries = 5                                          #und solange ein neuer Versuch angeboten, bis die Versuche aufgebraucht sind
+    for i in range(5, 0, -1):
+        if user_response != response and response_tries > 0:
+            print("Falsche Lösung!")
+            print(f"du hast noch {i} Versuche!")
             user_response = input("Deine Lösung: ")
-        else:
-            print("Starte das Programm erneut!")
-    else:
-        print("richtige Antwort! Super!")
+            response_tries -= 1
+        elif response_tries > 0:
+            print("Super! Das ist richtig!")
+            response_tries = 1
+
+
 
 ### Input ###
-response_task1 = "PI = 3.14"
 
 ### Calculations ###
 
 ### Output ###
-check_answer(response_task1)
+answer_task1 = "PI = 3.14"
+check_answer (answer_task1)
  
 
