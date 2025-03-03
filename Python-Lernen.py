ef724019d4095b40d010e6fb263f8b14198ed243
MoMe920 <moritzmezler@gmail.com>
2025-03-03 10:13:00 +0100

    create definitions for each unit and check the anwers with the code of Fabian

--- Python-Lernen.py
+++ Python-Lernen.py
@@ -45,8 +45,7 @@ while True: # repeats the query for the user input until a correct input is made
 
 
 
-
-
+"""
 ### Values ###
 
 ### Functions ###
@@ -65,14 +64,93 @@ def check_answer (response):                                    #überprüft, ob
         if response_tries == 0 and user_response != response:
             print("Du hast deine Versuche aufgebraucht! Starte das Programm erneut!")
             exit()
-
-
-
 ### Input ###
 
 ### Calculations ###
 
 ### Output ###
+"""
 
- 
+def check_answer(response):  # Überprüft, ob die Eingabe richtig ist
+    user_response = input("Deine Lösung: ")
+    response_tries = 4  # Maximale Anzahl der Versuche
+
+    for i in range(4, 0, -1):
+        if user_response != response and response_tries > 0:
+            print("Falsche Lösung!")
+            print(f"Du hast noch {i} Versuche!")
+            user_response = input("Deine Lösung: ")
+        elif response_tries > 0:
+                print("Super! Das ist richtig!")
+                response_tries = 0
+        if response_tries == 0 and user_response != response:
+            print("Du hast deine Versuche aufgebraucht! Starte das Programm erneut!")
+            exit()
+
+
+def beginner_unit():
+    print("""
+    Lerneinheit für Anfänger:
+    - Was sind Variablen und wie verwendest du sie?
+    - Wie erstellst du Schleifen in Python?
+    - Funktionen definieren und anwenden
+
+    Drücke eine beliebige Taste, um mit der Lerneinheit zu beginnen...
+    """)
+    input()
+
+    print("\nFrage 1: Was ist eine Variable in Python?")
+    print("1) Eine Funktion")
+    print("2) Ein Datentyp")
+    print("3) Ein Speicherort für Daten")
+    print("4) Ein Schleifen-Konstrukt")
+    check_answer("3")  # Die richtige Antwort ist "3"
+
+    print("\nFrage 2: Wie erstelle ich eine Schleife in Python?")
+    print("1) Mit einer Funktion")
+    print("2) Mit einer Bedingung")
+    print("3) Mit 'for' oder 'while'")
+    print("4) Mit 'if' und 'else'")
+    check_answer("3")  # Die richtige Antwort ist "3"
+
+    print("\nFrage 3: Wie definierst du eine Funktion in Python?")
+    print("1) Mit 'function'")
+    print("2) Mit 'def'")
+    print("3) Mit 'lambda'")
+    print("4) Mit 'func'")
+    check_answer("2")  # Die richtige Antwort ist "2"
+
+    print("\nGlückwunsch! Du hast die Lerneinheit abgeschlossen!")
+
+def intermediate_unit():
+    print("""
+    Lerneinheit für Mittelstufe:
+    - Listen und Dictionaries verstehen und verwenden
+    - Fehlerbehandlung mit try-except
+    - Funktionen mit mehreren Parametern und Rückgabewerten
+
+    Drücke eine beliebige Taste, um mit der Lerneinheit zu beginnen...
+    """)
+    input()
+
+def advenced_unit():
+    print("""
+    Lerneinheit für Fortgeschrittene:
+    - Objektorientierte Programmierung (OOP) in Python
+    - Arbeiten mit Datenstrukturen wie Bäumen und Graphen
+    - Implementierung und Analyse von Algorithmen
+
+    Drücke eine beliebige Taste, um mit der Lerneinheit zu beginnen...
+    """)
+    input()
+
+
+
+if learning_level == "1":
+    beginner_unit()
+elif learning_level == "2":
+    intermediate_unit()
+elif learning_level == "3":
+    advanced_unit()
+
 
